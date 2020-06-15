f97532a7b83929ab758b6997dee3479de5135778
Gabriel Brun <lydstyl@gmail.com>
2020-06-15 16:08:24 +0200

    position window to top left

--- resize.sh
+++ resize.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-set -- $(xwininfo -root| awk -F '[ :]+' '/ (Width|Height):/ { print $3 }')
-width=$1
-height=$2
+# set -- $(xwininfo -root| awk -F '[ :]+' '/ (Width|Height):/ { print $3 }')
+# width=$1
+# height=$2
 
 #wmctrl -r :ACTIVE: -e 0,-1,-1,$((width*90/100)),$((height*90/100))
 # wmctrl -r :ACTIVE: -e 0,-1,-1,$((1920)),$((1080))
-wmctrl -r :ACTIVE: -e 0,-1,-1,1920,1080
+wmctrl -r :ACTIVE: -e 0,0,0,1920,1080
