b45167226026c0fbf5fd6536621e3b24f5fcaf18
Gabriel <lydstyl@gmail.com>
2020-07-03 23:47:12 +0200

    add hd

--- resize.sh
+++ resize.sh
@@ -6,4 +6,6 @@
 
 #wmctrl -r :ACTIVE: -e 0,-1,-1,$((width*90/100)),$((height*90/100))
 # wmctrl -r :ACTIVE: -e 0,-1,-1,$((1920)),$((1080))
-wmctrl -r :ACTIVE: -e 0,0,0,1920,1080
+
+#wmctrl -r :ACTIVE: -e 0,0,0,1920,1080
+wmctrl -r :ACTIVE: -e 0,0,0,1280,720
